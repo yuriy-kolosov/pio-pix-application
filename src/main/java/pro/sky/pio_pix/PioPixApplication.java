b7dff11ea6c5be53c92a113b46a717ba5abc5e84
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2025-05-19 02:04:53 +0300

    pio-pix_application - начальный коммит

---
+++ src/main/java/pro/sky/pio_pix/PioPixApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.pio_pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/pio_pix/PioPixApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.pio_pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PioPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
